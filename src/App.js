421207597d81a2cbadc9d0f6be1da4574ec03ce7
Helen Dias <helendias.hd@gmail.com>
2020-06-28 18:43:29 -0300

    :sparkles: Add list of projects and button to add a project

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+import Header from './components/Header'
+
+function App() {
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web'])
+
+  function handleAddProject() {
+    setProjects([...projects, `Projeto ${Date.now()}`])
+  }
+
+  return(
+    <>
+      <Header title="Projects" />
+
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
+    </>
+  )
+}
+
+export default App
